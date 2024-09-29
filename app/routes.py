b773f93736259ca6fc0702887331541c6db53244
Akbar FItriawan <akbarfitriawan12@gmail.com>
2024-09-30 01:47:28 +0700

    layouting

--- app/routes.py
+++ app/routes.py
@@ -1,32 +1,59 @@
 from flask import Blueprint, render_template
 
+from datetime import datetime
+
 main_bp = Blueprint('main', __name__)
 
+nav_links = [
+        {'name': 'Home', 'url': 'main.home'},
+        {'name': 'About', 'url': 'main.about'},
+        {'name': 'Services', 'url': 'main.services'},
+        {'name': 'Why Choose', 'url': 'main.whyme'},
+        {'name': 'Portfolio', 'url': 'main.portfolio'},
+        {'name': 'Blog', 'url': 'main.blog'},
+        {'name': 'Contact', 'url': 'main.contact'}
+]
+
+@main_bp.app_context_processor
+def inject_globals():
+    return {
+        'nav_links': nav_links,
+        'current_year': datetime.now().year,
+        'owner_name': "Akbar Fitriawan"
+    }
+
+
 @main_bp.route('/')
+@main_bp.route('/home')
 def home():
-    return render_template('index.html', name="Home")
+    
+    return render_template(
+        'index.html',
+        name="Home"
+        )
 
 @main_bp.route('/about')
 def about():
-    return render_template('about.html', name="About")
+    
+    return render_template('about.html', nav_links=nav_links, name="About")
 
 @main_bp.route('/services')
 def services():
-    return render_template('services.html', name="Services")
+    return render_template('services.html', nav_links=nav_links, name="Services")
 
 @main_bp.route('/whyme')
-def why_me():
-    return render_template('whyme.html', name="Why me")
+def whyme():
+    return render_template('whyme.html', nav_links=nav_links, name="Why choose")
 
 @main_bp.route('/portfolio')
 def portfolio():
-    return render_template('portfolio.html', name="Portfolio")
+    return render_template('portfolio.html', nav_links=nav_links, name="Portfolio")
 
 @main_bp.route('/blog')
 def blog():
-    return render_template('blog.html', name="Blog")
+    return render_template('blog.html', nav_links=nav_links, name="Blog")
 
 @main_bp.route('/contact')
 def contact():
-    return render_template('contact.html', name="Contact")
+    return render_template('contact.html', nav_links=nav_links, name="Contact")
 
