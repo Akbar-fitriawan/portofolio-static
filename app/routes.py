75974b310b686284ddeae6baee5ece10e1ad9d94
Akbar FItriawan <akbarfitriawan12@gmail.com>
2024-10-03 21:32:30 +0700

    update routes

--- app/routes.py
+++ app/routes.py
@@ -4,20 +4,10 @@ from datetime import datetime
 
 main_bp = Blueprint('main', __name__)
 
-nav_links = [
-        {'name': 'Home', 'url': 'main.home'},
-        {'name': 'About', 'url': 'main.about'},
-        {'name': 'Services', 'url': 'main.services'},
-        {'name': 'Why Choose', 'url': 'main.whyme'},
-        {'name': 'Portfolio', 'url': 'main.portfolio'},
-        {'name': 'Blog', 'url': 'main.blog'},
-        {'name': 'Contact', 'url': 'main.contact'}
-]
 
 @main_bp.app_context_processor
 def inject_globals():
     return {
-        'nav_links': nav_links,
         'current_year': datetime.now().year,
         'owner_name': "Akbar Fitriawan"
     }
@@ -26,34 +16,40 @@ def inject_globals():
 @main_bp.route('/')
 @main_bp.route('/home')
 def home():
+
+    nav_links = [
+        {'name': 'Home', 'url': 'home'},
+        {'name': 'About', 'url': 'about'},
+        {'name': 'Services', 'url': 'services'},
+        {'name': 'Portfolio', 'url': 'portfolio'},
+        {'name': 'Contact', 'url': 'contact'}
+    ]
     
     return render_template(
         'index.html',
-        name="Home"
+        nav_links=nav_links
+
         )
 
-@main_bp.route('/about')
-def about():
+# @main_bp.route('/about')
+# def about():
     
-    return render_template('about.html', nav_links=nav_links, name="About")
+#     return render_template('about.html', nav_links=nav_links, name="About")
 
-@main_bp.route('/services')
-def services():
-    return render_template('services.html', nav_links=nav_links, name="Services")
+# @main_bp.route('/services')
+# def services():
+#     return render_template('services.html', nav_links=nav_links, name="Services")
 
-@main_bp.route('/whyme')
-def whyme():
-    return render_template('whyme.html', nav_links=nav_links, name="Why choose")
 
-@main_bp.route('/portfolio')
-def portfolio():
-    return render_template('portfolio.html', nav_links=nav_links, name="Portfolio")
+# @main_bp.route('/portfolio')
+# def portfolio():
+#     return render_template('portfolio.html', nav_links=nav_links, name="Portfolio")
 
-@main_bp.route('/blog')
-def blog():
-    return render_template('blog.html', nav_links=nav_links, name="Blog")
+# @main_bp.route('/blog')
+# def blog():
+#     return render_template('blog.html', nav_links=nav_links, name="Blog")
 
-@main_bp.route('/contact')
-def contact():
-    return render_template('contact.html', nav_links=nav_links, name="Contact")
+# @main_bp.route('/contact')
+# def contact():
+#     return render_template('contact.html', nav_links=nav_links, name="Contact")
 
