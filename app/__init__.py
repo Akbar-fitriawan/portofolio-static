634e3904c2bad86c82d104f0e09d7262001875e2
Akbar FItriawan <akbarfitriawan12@gmail.com>
2024-10-15 14:08:01 +0700

    Update some features

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask_mail import Mail
+# from flask_wtf.csrf import CSRFProtect  # Tambahkan import CSRF
+
+
+mail = Mail()
+# csrf = CSRFProtect()  # Inisialisasi CSRF protection
 
 def create_app():
     app = Flask(__name__)
+    
+        # Konfigurasi dari config.py
     app.config.from_object('config.Config')
 
+    # Inisialisasi mail dan csrf
+    mail.init_app(app)
+    # csrf.init_app(app)
+    
     # Import blueprints (untuk modularisasi jika dibutuhkan)
     from .routes import main_bp
     app.register_blueprint(main_bp)
--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_mail import Message
+# from .forms import ContactForm
+from config import Config
+from . import mail
+from datetime import datetime
 from utils import load_json_data
 
-from datetime import datetime
+# mail = Mail()
 
 main_bp = Blueprint('main', __name__)
-
 
 @main_bp.app_context_processor
 def inject_globals():
     return {
         'current_year': datetime.now().year,
         'owner_name': "Akbar Fitriawan",
-
     }
 
-
-@main_bp.route('/')
-@main_bp.route('/home')
+@main_bp.route('/', methods=['GET', 'POST'])
 def home():
-
     nav_links = [
         {'name': 'Home', 'url': 'home'},
         {'name': 'About', 'url': 'about'},
@@ -27,15 +27,48 @@ def home():
         {'name': 'Blog', 'url': 'blog'},
         {'name': 'Contact', 'url': 'contact'}
     ]
+    
     data = load_json_data('./app/static/data.json')
+
+
+    return render_template('index.html', 
+                        #    form=form, 
+                           nav_links=nav_links, 
+                           personal_details=data['personal_details'], 
+                           certificates=data['certificates'], 
+                           educations=data['educations'],
+                            experience=data['experiance']
+                           )
+
+
+
+# Route untuk form
+@main_bp.route('/send-message', methods=['POST'])
+def send_message():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        subject = request.form['subject']
+        message = request.form['message']
+
+    if not name or not email or not subject or not message:
+           flash('Semua kolom harus diisi!', 'danger')
+           return redirect(url_for('main.index'))
+
+    # Membuat pesan email
+    msg = Message(
+                subject=f"Message from {name}: {subject}",
+                sender=email,
+                recipients=['akbarfitriawan12@gmail.com'] # Email tujuan (Gmail kamu)
+            )
     
+    msg.body = f"From: {name} <{email}>\n\n{message}"
 
-    
-    return render_template(
-        'index.html',
-        nav_links=nav_links,
-        personal_details=data['personal_details'], 
-        certificates=data['certificates'],
-        educations=data['educations'])
+    # Mengirim email
+    try:
+        mail.send(msg)
+        flash('Message sent successfully!', 'success')
+    except Exception as e:
+        flash(f'Failed to send message. Error: {e}', 'danger')
 
-
+    return redirect(url_for('main.home'))
