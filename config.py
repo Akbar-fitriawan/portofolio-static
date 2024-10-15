0b031f66e0f25dc5c614f7b57f7c0130f0bf16e7
Akbar FItriawan <akbarfitriawan12@gmail.com>
2024-10-15 14:08:01 +0700

    Update some features

--- config.py
+++ config.py
@@ -1,8 +1,20 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv('.env')
 
 class Config:
+    # app secret key
     SECRET_KEY = os.getenv('SECRET_KEY')
-    DEBUG=True
+    # Mail konfigurasi
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT'))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS').lower() in ['true', '1', 't']
+    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL').lower() in ['true', '1', 't']
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+    
+
+
+    
