a878a8fc33faa32fdfd2599dd7790100e1055a37
Akbar FItriawan <akbarfitriawan12@gmail.com>
2024-10-18 00:22:20 +0700

    modify index

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_mail import Message
-# from .forms import ContactForm
+
 from config import Config
 from . import mail
 from datetime import datetime
@@ -17,12 +17,14 @@ def inject_globals():
         'owner_name': "Akbar Fitriawan",
     }
 
+
 @main_bp.route('/', methods=['GET', 'POST'])
+@main_bp.route('/send-message')
 def home():
     nav_links = [
         {'name': 'Home', 'url': 'home'},
         {'name': 'About', 'url': 'about'},
-        {'name': 'Services', 'url': 'services'},
+        # {'name': 'Services', 'url': 'services'},
         {'name': 'Portfolio', 'url': 'portfolio'},
         {'name': 'Blog', 'url': 'blog'},
         {'name': 'Contact', 'url': 'contact'}
@@ -32,43 +34,42 @@ def home():
 
 
     return render_template('index.html', 
-                        #    form=form, 
                            nav_links=nav_links, 
                            personal_details=data['personal_details'], 
                            certificates=data['certificates'], 
                            educations=data['educations'],
-                            experience=data['experiance']
-                           )
+                           experiance=data['experiance'])
 
 
 
-# Route untuk form
-@main_bp.route('/send-message', methods=['POST'])
-def send_message():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        subject = request.form['subject']
-        message = request.form['message']
+# # Route untuk form
+# @main_bp.route('/send-message', methods=['POST'])
+# def send_message():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         email = request.form['email']
+#         subject = request.form['subject']
+#         message = request.form['message']
 
-    if not name or not email or not subject or not message:
-           flash('Semua kolom harus diisi!', 'danger')
-           return redirect(url_for('main.index'))
+#     if not name or not email or not subject or not message:
+#            flash('Semua kolom harus diisi!', 'danger')
+#            return redirect(url_for('main.home'))
 
-    # Membuat pesan email
-    msg = Message(
-                subject=f"Message from {name}: {subject}",
-                sender=email,
-                recipients=['akbarfitriawan12@gmail.com'] # Email tujuan (Gmail kamu)
-            )
+#     # Membuat pesan email
+#     msg = Message(
+#                 subject=f"Message from {name}: {subject}",
+#                 sender=email,
+#                 recipients=['akbarfitriawan12@gmail.com'] # Email tujuan (Gmail kamu)
+#                 reply_to=['a']
+#             )
     
-    msg.body = f"From: {name} <{email}>\n\n{message}"
+#     msg.body = f"From: {name} <{email}>\n\n{message}"
 
-    # Mengirim email
-    try:
-        mail.send(msg)
-        flash('Message sent successfully!', 'success')
-    except Exception as e:
-        flash(f'Failed to send message. Error: {e}', 'danger')
+#     # Mengirim email
+#     try:
+#         mail.send(msg)
+#         flash('Message sent successfully!', 'success')
+#     except Exception as e:
+#         flash(f'Failed to send message. Error: {e}', 'danger')
 
-    return redirect(url_for('main.home'))
+#     return redirect(url_for('main.home'))
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,20 @@ module.exports = {
     "./app/static/src/**/*.js",
     "./node_modules/flowbite/**/*.js",
   ],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
       padding: "32px",
     },
 
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
